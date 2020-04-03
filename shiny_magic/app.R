7666a2e8b495f3ced1c029017088e6d6db10b3f4
Elias DeLeon <juggereggnog@gmail.com>
2020-04-03 16:01:37 -0700

    added titles to each tab

--- shiny_magic/app.R
+++ shiny_magic/app.R
@@ -1,19 +1,22 @@
 library(shiny)
 library(shinythemes)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(theme = shinytheme("cyborg"),
         
         navbarPage("This is Magic",
                     
             tabPanel("Model",
                  
-                 titlePanel("Breaking Down Tournament Decks")
+                 titlePanel("Model"),
+                 
+                 h3("Breaking Down Tournament Decks")
         ),
         
             tabPanel("Discussion",
                  
-                 titlePanel("Further Analysis")
+                 titlePanel("Discussion"),
+                 
+                 h3("Further Analysis")
         ),
         
             tabPanel("Takeaways",
@@ -23,16 +26,17 @@ ui <- fluidPage(theme = shinytheme("cyborg"),
         
             tabPanel("About",
                  
-                 titlePanel("A Background in Magic")
+                 titlePanel("About"),
+                 
+                 h3("A Background in Magic")
         )
     )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
 
     
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
